76fd382b28b6a0e3a60d3c84f35606830ecb414c
GregorioBaldari <gregorio.baldari@gmail.com>
2016-02-13 20:42:27 +0000

    Added UI-Router to public
    all the previous views in room have been removed for now

--- public/js/core.js
+++ public/js/core.js
@@ -1,10 +1,9 @@
 
 var weSizeItApp = angular.module('weSizeItApp', [
-        'ngRoute',
+        'ui.router',
         'chart.js',
         'userService',
         'appControllers',
-        'UserApp',
         'angulartics', 
         'angulartics.google.analytics'
     ]);
@@ -52,44 +51,76 @@ weSizeItApp.factory('socket', ['$rootScope', function ($rootScope) {
     };
 }]);
 
-weSizeItApp.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-    .when('/login', {
-        templateUrl: 'views/login.html',
-        public: true,
-        login: true,
-    })
-    .when('/signup', {
-        templateUrl: 'views/signup.html',
-        public: true,
-    })
-    .when('/room', {
-        templateUrl: 'views/room.html',
-        controller: 'mainViewCtrl',
-    })
-    .otherwise({
-        redirectTo: '/room',
-    });
-}]);
+weSizeItApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'views/landing.html'
+        })
+        
+        // nested list with custom controller
+        .state('login', {
+            url: '/list',
+            templateUrl: 'views/login.html'
+        })
+        
+        // nested list with just some random string data
+        .state('signup', {
+            url: '/signup',
+            templateUrl: 'views/signup.html'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('room', {
+            url: '/room',
+            templateUrl: 'views/room.html'
+        });
+        
+});
+//
+//weSizeItApp.config(['$routeProvider', function ($routeProvider) {
+//    $routeProvider
+//    .when('/login', {
+//        templateUrl: 'views/login.html',
+//        public: true,
+//        login: true,
+//    })
+//    .when('/signup', {
+//        templateUrl: 'views/signup.html',
+//        public: true,
+//    })
+//    .when('/room', {
+//        templateUrl: 'views/room.html',
+//        controller: 'mainViewCtrl',
+//    })
+//    .otherwise({
+//        redirectTo: '/room',
+//    });
+//}]);
 
 //This stuff is for login
 //The btoa() function may not be supported by all browsers.
 //btoa() is used to autoriz the backend API
 //weSizeItApp.run(function($rootScope, user,$http, PBs, UserService, UserApp) {
-weSizeItApp.run(function($rootScope, user,$http, UserService, UserApp) {
-	user.init({ appId: '563f8a3e36901' });
-    $rootScope.$on('user.login', function () {
-        console.log('User Token: ' + user.token());
-        $http.defaults.headers.common.Authorization = 'Basic ' + btoa(':' + user.token());
-        UserApp.User.get({
-            'user_id' : user.user_id
-        }, function( error, result) {
-            if(error) console.log('UserApp Error: ' + error);
-            UserService.registerUsers(result[0]);
-        });
-    });
-    $rootScope.$on('user.logout', function () {
-        $http.defaults.headers.common.Authorization = null;
-        //UserService.setUser(undefined);
-    });
+weSizeItApp.run(function($rootScope, $http, UserService) {
+//	user.init({ appId: '563f8a3e36901' });
+//    $rootScope.$on('user.login', function () {
+//        console.log('User Token: ' + user.token());
+//        $http.defaults.headers.common.Authorization = 'Basic ' + btoa(':' + user.token());
+//        UserApp.User.get({
+//            'user_id' : user.user_id
+//        }, function( error, result) {
+//            if(error) console.log('UserApp Error: ' + error);
+//            UserService.registerUsers(result[0]);
+//        });
+//    });
+//    $rootScope.$on('user.logout', function () {
+//        $http.defaults.headers.common.Authorization = null;
+//        //UserService.setUser(undefined);
+//    });
 });
